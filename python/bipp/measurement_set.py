e5c46a3ebe69f4a1764fcae59fdf599a694f1c15
Kincaidr <kincaidr@ymail.com>
2024-10-16 15:03:49 +0200

    shreyams measurement set py file

--- python/bipp/measurement_set.py
+++ python/bipp/measurement_set.py
@@ -279,6 +279,7 @@ class MeasurementSet:
             beam_id_0 = sub_table.getcol("ANTENNA1")  # (N_entry,)
             beam_id_1 = sub_table.getcol("ANTENNA2")  # (N_entry,)
             data_flag = sub_table.getcol("FLAG")  # (N_entry, N_channel, 4)
+            print('Flag column', data_flag)
             data = sub_table.getcol(column)  # (N_entry, N_channel, 4)
 
             try:
@@ -287,12 +288,14 @@ class MeasurementSet:
                 weight_spectrum = None
 
             # We only want XX and YY correlations
-            data = np.average(data[:, :, [0, 3]], axis=2)[:, channel_id]
-            data_flag = np.any(data_flag[:, :, [0, 3]], axis=2)[:, channel_id]
+            print('before',data)
+            data = np.average(data[:, :, [0, 1]], axis=2)[:, channel_id]
+            print('after0',data)
+            data_flag = np.any(data_flag[:, :, [0, 1]], axis=2)[:, channel_id]
 
             # Set broken visibilities to 0
             data[data_flag] = 0
-
+            print('after',data)
             # DataFrame description of visibility data.
             # Each column represents a different channel.
             S_full_idx = pd.MultiIndex.from_arrays((beam_id_0, beam_id_1), names=("B_0", "B_1"))
@@ -330,7 +333,7 @@ class MeasurementSet:
 
             if weight_spectrum is not None:
                 # Mimic WSClean
-                weight = np.min(weight_spectrum[:, :, [0, 3]], axis=2)[:, channel_id]
+                weight = np.min(weight_spectrum[:, :, [0, 1]], axis=2)[:, channel_id]
                 weight[data_flag] = 0
                 W_full = pd.DataFrame(data=weight, columns=channel_id, index=S_full_idx)
                 W_trunc = W_full.drop(index=index_to_drop)
@@ -698,5 +701,91 @@ class SKALowMeasurementSet(MeasurementSet):
             direction = self.field_center
             beam_config = [(_, _, direction) for _ in beam_id]
             self._beamformer = beamforming.MatchedBeamformerBlockIdentity(beam_config)
+
+        return self._beamformer
+
+class GenericMeasurementSet(MeasurementSet):
+    """
+    Generic Measurement Set reader.
+    """
+    def __init__(self, file_name):
+        """
+        Parameters
+        ----------
+        file_name : str
+            Name of the MS file.
+        """
+        self._nstation = None
+        super().__init__(file_name)
+
+    @property
+    def instrument(self):
+        """
+        Returns
+        -------
+        :py:class:`~pypeline.phased_array.instrument.EarthBoundInstrumentGeometryBlock`
+            Instrument position computer.
+        """
+        if self._instrument is None:
+            # Following the MS file specification from https://casa.nrao.edu/casadocs/casa-5.1.0/reference-material/measurement-set,
+            # the ANTENNA sub-table specifies the antenna geometry.
+            # Some remarks on the required fields:
+            # - POSITION: absolute station positions in ITRF coordinates.
+            # - ANTENNA_ID: equivalent to STATION_ID field `InstrumentGeometry.index[0]`
+            #               This field is NOT present in the ANTENNA sub-table, but is given
+            #               implicitly by its row-ordering.
+            #               In other words, the station corresponding to ANTENNA1=k in the MAIN
+            #               table is described by the k-th row of the ANTENNA sub-table.
+            query = f"select POSITION from {self._msf}::ANTENNA"
+            table = ct.taql(query)
+            station_mean = table.getcol("POSITION")
+
+            N_station = len(station_mean)
+            station_id = np.arange(N_station)
+            cfg_idx = pd.MultiIndex.from_product(
+                [station_id, [0]], names=("STATION_ID", "ANTENNA_ID")
+            )
+            cfg = pd.DataFrame(data=station_mean, columns=("X", "Y", "Z"), index=cfg_idx)
+
+            XYZ = instrument.InstrumentGeometry(xyz=cfg.values, ant_idx=cfg.index)
+
+            self._instrument = instrument.EarthBoundInstrumentGeometryBlock(XYZ)
+        return self._instrument
+        
+    def AntennaNumber(self):
+        """
+        Returns 
+        -------
+        Number of stations present in MS file (int)
+        """
+        query = f"select POSITION from {self._msf}::ANTENNA"
+        table = ct.taql(query)
+        station_mean = table.getcol("POSITION")
+        self._nstation = len(station_mean)
+        return self._nstation
+
+
+
+    @property
+    def beamformer(self):
+        """
+        Each dataset has been beamformed in a specific way.
+        This property outputs the correct beamformer to compute the beamforming weights.
+
+        Returns
+        -------
+        :py:class:`~pypeline.phased_array.beamforming.MatchedBeamformerBlock`
+            Beamweight computer.
+        """
+        if self._beamformer is None:
+            # MWA does not do any beamforming.
+            # Given the single-antenna station model in MS files from MWA, this can be seen as
+            # Matched-Beamforming, with a single beam output per station.
+            XYZ = self.instrument._layout
+            beam_id = np.unique(XYZ.index.get_level_values("STATION_ID"))
+
+            direction = self.field_center
+            beam_config = [(_, _, direction) for _ in beam_id]
+            self._beamformer = beamforming.MatchedBeamformerBlock(beam_config)
 
         return self._beamformer
